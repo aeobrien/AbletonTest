cfc8befa585219dfbc23b0fee08795f2b682598e
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-07-23 18:28:36 +0100

    Initial Commit

---
+++ AbletonTest/AbletonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbletonTestApp.swift
+//  AbletonTest
+//
+//  Created by Aidan O'Brien on 23/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AbletonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
